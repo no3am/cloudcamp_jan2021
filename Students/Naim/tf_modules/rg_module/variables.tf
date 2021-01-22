bf21e8b8feda82d1fd67bff85cbe9aedbea1e1ba
Naim Salameh <naim.salameh@consensys.net>
2021-01-22 09:42:58 +0200

    adding resource group module

---
+++ Students/Naim/tf_modules/rg_module/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  type        = string
+}
+
+variable "rg_name" {
+  description = "(Required) The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  type        = string
+}
